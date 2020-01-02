7c6f640f6ecc8c148cb65685c2e87a58c3a51838
Aaron Glenn <AaronEdwardGlenn@gmail.com>
2020-01-02 15:32:53 -0800

    added validpassword/email auth

--- User.js
+++ User.js
@@ -18,7 +18,33 @@ const schema = new mongoose.Schema(
         ret.passwordHash; 
       }   
     }
+  });
+
+schema.virtual('password').set(function(password) {
+  const passwordHash = bcrypt.hashSync(password);
+  this.passwordHash = passwordHash;
+});
+
+schema.methods.authToken = function() {
+  return jest.sign(this.toJSON(), process.env.APP_SECRET, {
+    expiresIn: '24h'
+  }); 
+};
+
+schema.statics.authorize = async function({ email, password }) {
+  const user = await this.findOne ({ email }); 
+  if(!user) {
+    const err = new Error('This email is invalid'); 
+    err.status = 401; 
+    throw err; 
   }
-);
+  const validPassword = await bcrypt.compare(password, user.passwordHash); 
+  if(!validPassword){
+    const err = new Error('That password is not correct. Sort it out.'); 
+    err.status = 401; 
+    throw err; 
+  }
+  return user; 
+};
 
 module.exports = mongoose.model('User', schema); 
