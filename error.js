023cbab5369709975a66726bab8e6e95c3f92541
Aaron Glenn <AaronEdwardGlenn@gmail.com>
2020-01-02 16:13:43 -0800

    added err.js

---
+++ error.js
@@ -0,0 +1,16 @@
+/* eslint-disable no-console */
+/* eslint-disable no-unused-vars */
+const mongoose = require('mongoose'); 
+
+module.exports = (err, req, res, next) => {
+  let status = err.status || 500; 
+  
+  if(err instanceof mongoose.Error.ValidationError || err instanceof mongoose.Error.CastError) {
+    status = 400; 
+  }
+  res.status(status); 
+  console.log(err);
+  
+  res.send({ status, message: err.message }); 
+  
+};
